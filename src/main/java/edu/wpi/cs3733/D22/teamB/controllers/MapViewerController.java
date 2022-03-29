43f67bfecc9badea82b8706bec1cb7aa9b74ebd4
Aidan  MacNevin <ahmacnevin@wpi.edu>
2022-03-29 17:24:52 -0400

    Added Menu Bar to Map Viewer

--- src/main/java/edu/wpi/cs3733/D22/teamB/controllers/MapViewerController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/MapViewerController.java
@@ -4,16 +4,11 @@ import com.jfoenix.controls.JFXButton;
 import java.awt.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 import javax.swing.*;
 
-public class MapViewerController {
+public class MapViewerController extends MenuBarController {
   @FXML private JFXButton upButton;
   @FXML private JFXButton downButton;
   @FXML private ImageView mapImage;
@@ -62,16 +57,5 @@ public class MapViewerController {
         break;
     }
     floorLevel--;
-  }
-
-  @FXML
-  void goToHomepage(ActionEvent event) throws Exception {
-    Parent homepageRoot =
-        FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D22/teamB/views/homepage.fxml"));
-    Scene homepageScene = new Scene(homepageRoot);
-
-    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    window.setScene(homepageScene);
-    window.show();
   }
 }
