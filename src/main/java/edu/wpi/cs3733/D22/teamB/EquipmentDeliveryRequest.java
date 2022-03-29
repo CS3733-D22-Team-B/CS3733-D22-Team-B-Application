e29c03a84332d4b9cec0dc7e3087172da14057c2
spateyz <94015210+spateyz@users.noreply.github.com>
2022-03-29 18:12:56 -0400

    added Equipment Delivery Request container class wip

---
+++ src/main/java/edu/wpi/cs3733/D22/teamB/EquipmentDeliveryRequest.java
@@ -0,0 +1,19 @@
+package edu.wpi.cs3733.D22.teamB;
+
+public class EquipmentDeliveryRequest {
+    private String equipment;
+    // TODO: location l;
+    public EquipmentDeliveryRequest(String equip)
+    {
+        equipment = equip;
+    }
+    public void setEquipment(String equip)
+    {
+        equipment = equip;
+    }
+    public String getEquipment()
+    {
+        return equipment;
+    }
+    // TODO: location object get/set
+}
