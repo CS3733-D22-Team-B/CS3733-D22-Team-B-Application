3a69393fd5f51048607f83302ab2e0a18e99b24a
spateyz <94015210+spateyz@users.noreply.github.com>
2022-03-29 12:44:35 -0400

    Added back button to MapView page

--- src/main/java/edu/wpi/cs3733/D22/teamB/controllers/MapViewerController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/MapViewerController.java
@@ -1,10 +1,16 @@
 package edu.wpi.cs3733.D22.teamB.controllers;
 
 import com.jfoenix.controls.JFXButton;
+import java.awt.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import javax.swing.*;
 
 public class MapViewerController {
@@ -56,5 +62,16 @@ public class MapViewerController {
         break;
     }
     floorLevel--;
+  }
+
+  @FXML
+  void goToHomepage(ActionEvent event) throws Exception {
+    Parent homepageRoot =
+        FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D22/teamB/views/homepage.fxml"));
+    Scene homepageScene = new Scene(homepageRoot);
+
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(homepageScene);
+    window.show();
   }
 }
