e1c9a2b30d8120d58f787d3d81c7181bbc38ca15
Chase Miller <cpmills26@gmail.com>
2022-03-29 18:22:20 -0400

    Add connection between ends

--- src/main/java/edu/wpi/cs3733/D22/teamB/Main.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/Main.java
@@ -1,8 +1,25 @@
 package edu.wpi.cs3733.D22.teamB;
 
+import edu.wpi.cs3733.D22.teamB.databases.DatabaseInitializer;
+
 public class Main {
 
   public static void main(String[] args) {
+        System.out.println("-------Embedded Apache Derby Connection Testing-------");
+        try {
+          Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException e) {
+          System.out.println("Apache Derby Driver not found. Add the classpath to your module.");
+          System.out.println("For IntelliJ do the following:");
+          System.out.println("File | Project Structure, Modules, Dependency tab");
+          System.out.println("Add by clicking on the green plus icon on the right of the window");
+          System.out.println("Select JARs or directories. Go to the folder where the database JAR is located");
+          System.out.println("Click OK, now you can compile your program and run it.");
+          e.printStackTrace();
+          return;
+        }
+
+        DatabaseInitializer dB = new DatabaseInitializer();
     App.launch(App.class, args);
   }
 }
