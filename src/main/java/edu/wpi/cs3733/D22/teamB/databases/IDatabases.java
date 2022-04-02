d23e510e71e3e953e6f618c0a3cf6ece55b54ed1
Marek Garbaczonek <msgarbaczonek@gmail.com>
2022-04-02 17:10:23 -0400

    IDatabases Interface and Deleted Other Useless Interfaces

--- src/main/java/edu/wpi/cs3733/D22/teamB/databases/IDatabases.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/databases/IDatabases.java
@@ -2,16 +2,16 @@ package edu.wpi.cs3733.D22.teamB.databases;
 
 import java.util.LinkedList;
 
-public interface IDatabases <L>{
-    public LinkedList<L> list();
+public interface IDatabases<L> {
+  public LinkedList<L> list();
 
-    public LinkedList<L> searchFor(String input);
+  public LinkedList<L> searchFor(String input);
 
-    public int update(L locObj);
+  public int update(L locObj);
 
-    public int add(L locObj);
+  public int add(L locObj);
 
-    public int delete(L locObj);
+  public int delete(L locObj);
 
-    public void quit();
+  public void quit();
 }
