02bb8761d644b98ecfa18c2cc259f4cfe8f6ee66
spateyz <94015210+spateyz@users.noreply.github.com>
2022-04-02 15:58:38 -0400

    added hide/show functions

--- src/main/java/edu/wpi/cs3733/D22/teamB/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/MapEditorController.java
@@ -1,5 +1,51 @@
 package edu.wpi.cs3733.D22.teamB.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MapEditorController extends MapViewerController {
+
+  @FXML private JFXButton addButton;
+  @FXML private TextField addLocationName;
+  @FXML private Button cancelButton;
+  @FXML private Button confirmButton;
+  @FXML private Label addLabel;
+  @FXML private ComboBox<String> typeDropdown;
+  @FXML private JFXButton editButton;
+  @FXML private JFXButton removeButton;
+  @FXML private ComboBox<String> locationsDropdown;
+  @FXML private Button editEnableButton;
+  @FXML private TextField editNameField;
+  @FXML private Button deleteButton;
+  @FXML private Label warningLabel;
+
+  @FXML
+  public void hideButtons() {
+    addButton.setDisable(true);
+    addButton.setVisible(false);
+
+    editButton.setDisable(true);
+    editButton.setVisible(false);
+
+    removeButton.setDisable(true);
+    removeButton.setVisible(false);
+  }
+
+  @FXML
+  public void showButtons() {
+    addButton.setDisable(false);
+    addButton.setVisible(true);
+
+    editButton.setDisable(false);
+    editButton.setVisible(true);
+
+    removeButton.setDisable(false);
+    removeButton.setVisible(true);
+  }
+
 
 }
