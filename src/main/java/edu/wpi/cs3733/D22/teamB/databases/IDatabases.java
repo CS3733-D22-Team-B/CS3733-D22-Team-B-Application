beb625be3d71decf6cfa74d3b5de80a81be813ec
Nathan Wong <93850704+nk-wong@users.noreply.github.com>
2022-04-03 14:35:08 -0400

    Working generalized interface

--- src/main/java/edu/wpi/cs3733/D22/teamB/databases/IDatabases.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/databases/IDatabases.java
@@ -3,13 +3,13 @@ package edu.wpi.cs3733.D22.teamB.databases;
 import java.util.LinkedList;
 
 public interface IDatabases<L> {
-    public LinkedList<L> list();
+  public LinkedList<L> list();
 
-    public int update(L locObj);
+  public int update(L locObj);
 
-    public int add(L locObj);
+  public int add(L locObj);
 
-    public int delete(L locObj);
+  public int delete(L locObj);
 
-    public void quit();
+  public void quit();
 }
