de9d25b53b623e510429b680a3a2c71fca2058c2
Nathan Wong <93850704+nk-wong@users.noreply.github.com>
2022-04-03 17:06:27 -0400

    Refactored employee and equipment request (current solution does not work)

--- src/main/java/edu/wpi/cs3733/D22/teamB/databases/EquipmentRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/databases/EquipmentRequest.java
@@ -4,9 +4,12 @@ public class EquipmentRequest {
   private String requestID;
   private String type;
   private String employeeID;
+  private Employee employee;
   private String locationID;
+  private Location location;
   private String status;
   private String equipmentID;
+  private MedicalEquipment medicalEquipment;
   private String notes;
 
   EquipmentRequest(
@@ -30,7 +33,7 @@ public class EquipmentRequest {
     return requestID;
   }
 
-  public void setRequestID(String requestID) {
+  private void setRequestID(String requestID) {
     this.requestID = requestID;
   }
 
@@ -50,12 +53,32 @@ public class EquipmentRequest {
     this.employeeID = employeeID;
   }
 
+  public Employee getEmployee() {
+    EmployeeDB employeeDB = EmployeeDB.getInstance();
+    employee = employeeDB.getByID(employeeID);
+    return employee;
+  }
+
+  public void setEmployee(Employee e) {
+    setEmployeeID(e.getEmployeeID());
+  }
+
   public String getLocationID() {
     return locationID;
   }
 
   public void setLocationID(String locationID) {
     this.locationID = locationID;
+  }
+
+  public Location getLocation() {
+    LocationsDB locationsDB = LocationsDB.getInstance();
+    location = locationsDB.getByID(locationID);
+    return location;
+  }
+
+  public void setLocation(Location l) {
+    setLocationID(l.getNodeID());
   }
 
   public String getStatus() {
@@ -74,6 +97,16 @@ public class EquipmentRequest {
     this.equipmentID = equipmentID;
   }
 
+  public MedicalEquipment getMedicalEquipment() {
+    MedicalEquipmentDB medicalEquipmentDB = MedicalEquipmentDB.getInstance();
+    medicalEquipment = medicalEquipmentDB.getByID(equipmentID);
+    return medicalEquipment;
+  }
+
+  public void setMedicalEquipment(MedicalEquipment me) {
+    setEquipmentID(me.getEquipmentID());
+  }
+
   public String getNotes() {
     return notes;
   }
@@ -81,4 +114,60 @@ public class EquipmentRequest {
   public void setNotes(String notes) {
     this.notes = notes;
   }
+
+  ///////////////////EMPLOYEE GETTERS////////////////////
+  public String getLastName() {
+    return employee.getLastName();
+  }
+
+  public String getFirstName() {
+    return employee.getFirstName();
+  }
+
+  public String getDepartment() {
+    return employee.getDepartment();
+  }
+
+  public String getPosition() {
+    return employee.getPosition();
+  }
+
+  ///////////////////LOCATION GETTERS////////////////////
+  public int getXCoord() {
+    return location.getXCoord();
+  }
+
+  public int getYCoord() {
+    return location.getYCoord();
+  }
+
+  public String getFloor() {
+    return location.getFloor();
+  }
+
+  public String getBuilding() {
+    return location.getBuilding();
+  }
+
+  public String getNodeType() {
+    return location.getNodeType();
+  }
+
+  public String getLongName() {
+    return location.getLongName();
+  }
+
+  public String getShortName() {
+    return location.getShortName();
+  }
+
+  ///////////////////MEDICAL EQUIPMENT GETTERS////////////////////
+  public String getNodeID() {
+    return medicalEquipment.getNodeID();
+  }
+
+
+
+
+
 }
