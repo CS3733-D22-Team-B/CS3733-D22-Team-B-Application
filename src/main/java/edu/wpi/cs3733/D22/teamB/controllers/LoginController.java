fafb822150b34f805f9766adbe610cd05c7ba087
ethanmoynihan <49491805+ethanmoynihan@users.noreply.github.com>
2022-04-04 20:40:53 -0400

    login page now displays text if login invalid

--- src/main/java/edu/wpi/cs3733/D22/teamB/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/LoginController.java
@@ -1,13 +1,17 @@
 package edu.wpi.cs3733.D22.teamB.controllers;
 
+import edu.wpi.cs3733.D22.teamB.databases.Employee;
+import edu.wpi.cs3733.D22.teamB.databases.EmployeeDB;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController extends MenuBarController {
   @FXML private TextField usernameField;
   @FXML private PasswordField passwordBox;
+  @FXML private Label loginFail;
   @FXML private Button loginButton;
 
   private String username;
@@ -23,5 +27,17 @@ public class LoginController extends MenuBarController {
 
   public void login() {
     // Login functionality
+    EmployeeDB emp = EmployeeDB.getInstance();
+    Employee employee = emp.getEmployee(username);
+
+    if (employee.getPassword().equals(password))
+    {
+    //go to homepage
+    }
+    else
+    {
+      loginFail.setText("Invalid login");
+    }
+
   }
 }
