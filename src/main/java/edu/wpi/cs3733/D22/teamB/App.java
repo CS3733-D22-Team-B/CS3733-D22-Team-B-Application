65d6a4ab1bb980a255aed2256239af66d08505f7
Chase Miller <cpmills26@gmail.com>
2022-04-07 19:06:38 -0400

    Add Requests from database to Request Queue

--- src/main/java/edu/wpi/cs3733/D22/teamB/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/App.java
@@ -1,6 +1,8 @@
 package edu.wpi.cs3733.D22.teamB;
 
-import edu.wpi.cs3733.D22.teamB.databases.Employee;
+import edu.wpi.cs3733.D22.teamB.databases.*;
+import edu.wpi.cs3733.D22.teamB.requests.Request;
+import edu.wpi.cs3733.D22.teamB.requests.RequestQueue;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedList;
 
 @Slf4j
 public class App extends Application {
@@ -20,12 +24,30 @@ public class App extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    //TODO: Switch back to login as landing page
-    Parent root = FXMLLoader.load(getClass().getResource("views/interactiveMap.fxml"));
-    Scene scene = new Scene(root);
-    primaryStage.setScene(scene);
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+    primaryStage.setTitle("Login");
+    primaryStage.setScene(new Scene(root));
     primaryStage.initStyle(StageStyle.UNDECORATED);
     primaryStage.show();
+
+    // import requests from database
+    EquipmentRequestDB equipmentRequestDB = EquipmentRequestDB.getInstance();
+    LabRequestsDB labRequestsDB = LabRequestsDB.getInstance();
+    ServiceRequestsDB serviceDB = ServiceRequestsDB.getInstance();
+
+    LinkedList<EquipmentRequest> equipmentRequests = equipmentRequestDB.list();
+    LinkedList<LabRequest> labRequests = labRequestsDB.list();
+    LinkedList<Request> serviceRequests = serviceDB.list();
+
+    for (EquipmentRequest request : equipmentRequests) {
+      RequestQueue.addRequest(request);
+    }
+    for (LabRequest request : labRequests) {
+      RequestQueue.addRequest(request);
+    }
+    for (Request request : serviceRequests) {
+      RequestQueue.addRequest(request);
+    }
   }
 
   @Override
