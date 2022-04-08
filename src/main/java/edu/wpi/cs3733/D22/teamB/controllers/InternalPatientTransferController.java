0e3e79b296229ca01ab31ac58b47073186087d49
spateyz <94015210+spateyz@users.noreply.github.com>
2022-04-08 17:31:17 -0400

    added patient room filter to internal patient transfer controller

--- src/main/java/edu/wpi/cs3733/D22/teamB/controllers/InternalPatientTransferController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/InternalPatientTransferController.java
@@ -1,9 +1,15 @@
 package edu.wpi.cs3733.D22.teamB.controllers;
 
+import edu.wpi.cs3733.D22.teamB.databases.Location;
+import edu.wpi.cs3733.D22.teamB.databases.LocationsDB;
+import edu.wpi.cs3733.D22.teamB.databases.Patient;
+import edu.wpi.cs3733.D22.teamB.databases.PatientsDB;
 import edu.wpi.cs3733.D22.teamB.requests.InternalPatientTransferRequest;
 import edu.wpi.cs3733.D22.teamB.requests.RequestQueue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import java.util.LinkedList;
 
 public class InternalPatientTransferController extends PatientAndLocationBasedRequestController {
   @FXML
@@ -12,6 +18,44 @@ public class InternalPatientTransferController extends PatientAndLocationBasedRe
     if (!patientName.equals("") && !locationName.equals("")) {
       submitButton.setDisable(false);
     }
+  }
+  @Override
+  public void initialize()
+  {
+    locationsDAO = LocationsDB.getInstance();
+    LinkedList<Location> locations = locationsDAO.list();
+
+    for (Location location : locations) {
+      if(location.getNodeType().equals("PATI"))
+      {
+        switch (location.getFloor()) {
+          case "3":
+            locationsF3.add(location.getLongName());
+            break;
+          case "2":
+            locationsF2.add(location.getLongName());
+            break;
+          case "1":
+            locationsF1.add(location.getLongName());
+            break;
+          case "L1":
+            locationsFL1.add(location.getLongName());
+            break;
+          case "L2":
+            locationsFL2.add(location.getLongName());
+            break;
+        }
+      }
+    }
+    locationName = "";
+    floor = "";
+
+    patientsDB = PatientsDB.getInstance();
+
+    for (Patient patient : patientsDB.list()) {
+      patientInput.getItems().add(patient.getOverview());
+    }
+    patientName = patientInput.getValue();
   }
 
   @FXML
