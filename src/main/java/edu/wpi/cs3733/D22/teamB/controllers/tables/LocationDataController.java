d82202c4625f686542a247539f8e4554d68172c6
Chase Miller <cpmills26@gmail.com>
2022-04-11 13:21:20 -0400

    Fixed some front-end functionality

---
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/tables/LocationDataController.java
@@ -0,0 +1,56 @@
+package edu.wpi.cs3733.D22.teamB.controllers.tables;
+
+import edu.wpi.cs3733.D22.teamB.controllers.MenuBarController;
+import edu.wpi.cs3733.D22.teamB.databases.Location;
+import edu.wpi.cs3733.D22.teamB.databases.LocationsDB;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class LocationDataController extends MenuBarController implements Initializable {
+  @FXML TableView<edu.wpi.cs3733.D22.teamB.databases.Location> locationTable;
+  @FXML TableColumn<edu.wpi.cs3733.D22.teamB.databases.Location, String> columnName;
+  @FXML TableColumn<edu.wpi.cs3733.D22.teamB.databases.Location, String> columnX;
+  @FXML TableColumn<edu.wpi.cs3733.D22.teamB.databases.Location, String> columnY;
+  @FXML TableColumn<edu.wpi.cs3733.D22.teamB.databases.Location, String> columnFloor;
+  @FXML TableColumn<edu.wpi.cs3733.D22.teamB.databases.Location, String> columnBuilding;
+  @FXML private Button downloadButton;
+  @FXML private TextField downloadText;
+  @FXML private Label errorLabel;
+
+  private ObservableList<Location> locations = FXCollections.observableArrayList();
+  private LocationsDB dao;
+
+  @Override
+  public void initialize(URL loc, ResourceBundle resources) {
+    columnName.setCellValueFactory(new PropertyValueFactory<>("longName"));
+    columnX.setCellValueFactory(new PropertyValueFactory<>("xCoord"));
+    columnY.setCellValueFactory(new PropertyValueFactory<>("yCoord"));
+    columnFloor.setCellValueFactory(new PropertyValueFactory<>("floor"));
+    columnBuilding.setCellValueFactory(new PropertyValueFactory<>("building"));
+
+    dao = LocationsDB.getInstance();
+    LinkedList<edu.wpi.cs3733.D22.teamB.databases.Location> locs = dao.list();
+    for (edu.wpi.cs3733.D22.teamB.databases.Location location : locs) {
+      locations.add(location);
+    }
+
+    locationTable.setItems(locations);
+  }
+
+  @FXML
+  public void download() {
+    if (downloadText.getText().equals("")) {
+      errorLabel.setText("Please enter filename");
+    } else {
+      dao.downloadCSV(downloadText.getText());
+      errorLabel.setText("File downloaded");
+    }
+  }
+}
---
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/tables/ProfileController.java
@@ -0,0 +1,109 @@
+package edu.wpi.cs3733.D22.teamB.controllers.tables;
+
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.D22.teamB.App;
+import edu.wpi.cs3733.D22.teamB.controllers.MenuBarController;
+import edu.wpi.cs3733.D22.teamB.databases.Employee;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ProfileController extends MenuBarController {
+
+  @FXML private Label nameLabel;
+  @FXML private Label positionLabel;
+  @FXML private Label idLabel;
+  @FXML private Label usernameLabel;
+  @FXML private Label passwordLabel;
+  @FXML private JFXButton viewPasswordButton;
+  @FXML private JFXButton changePasswordButton;
+  @FXML private Label changePasswordLabel;
+  @FXML private TextField newPasswordField;
+  @FXML private TextField confirmPasswordField;
+  @FXML private Label errorLabel;
+
+  private String password;
+  private String hiddenPassword;
+
+  @FXML
+  public void initialize() {
+    Employee currentUser = App.currentUser;
+
+    nameLabel.setText(currentUser.getFirstName() + " " + currentUser.getLastName());
+    positionLabel.setText(currentUser.getPosition());
+    idLabel.setText(currentUser.getEmployeeID());
+    usernameLabel.setText("Username: " + currentUser.getUsername());
+
+    password = currentUser.getPassword();
+
+    hiddenPassword = "";
+    for (int i = 0; i < password.length(); i++) {
+      hiddenPassword += "\u2022";
+    }
+
+    passwordLabel.setText("Password: " + hiddenPassword);
+  }
+
+  public void togglePassword(ActionEvent actionEvent) {
+    if (viewPasswordButton.getText().equals("View Password")) {
+      togglePassword(true);
+      viewPasswordButton.setText("Hide Password");
+    } else {
+      togglePassword(false);
+      viewPasswordButton.setText("View Password");
+    }
+  }
+
+  public void openChangePasswordDisplay(ActionEvent actionEvent) {
+    if (changePasswordButton.isVisible()) {
+      toggleChangePasswordDisplay(false);
+    } else {
+      toggleChangePasswordDisplay(true);
+    }
+  }
+
+  public void changePassword(ActionEvent actionEvent) {
+    String newPassword = newPasswordField.getText();
+    String confirmPassword = confirmPasswordField.getText();
+
+    if (newPassword.equals(password) || confirmPassword.equals(password)) {
+      errorLabel.setText("New password cannot be the same as the old password.");
+    } else if (!newPassword.equals(confirmPassword)) {
+      errorLabel.setText("Passwords do not match");
+    } else {
+      App.currentUser.setPassword(newPassword);
+      password = newPassword;
+      hiddenPassword = "";
+      for (int i = 0; i < password.length(); i++) {
+        hiddenPassword += "\u2022";
+      }
+      if (viewPasswordButton.getText().equals("View Password")) {
+        togglePassword(false);
+      } else {
+        togglePassword(true);
+      }
+      toggleChangePasswordDisplay(false);
+
+      newPasswordField.setText("");
+      confirmPasswordField.setText("");
+      errorLabel.setText("");
+    }
+  }
+
+  private void togglePassword(boolean visible) {
+    if (visible) {
+      passwordLabel.setText("Password: " + password);
+    } else {
+      passwordLabel.setText("Password: " + hiddenPassword);
+    }
+  }
+
+  private void toggleChangePasswordDisplay(boolean visible) {
+    changePasswordButton.setVisible(visible);
+    changePasswordLabel.setVisible(visible);
+    newPasswordField.setVisible(visible);
+    confirmPasswordField.setVisible(visible);
+    errorLabel.setVisible(visible);
+  }
+}
