703730d4c9d1018e654ea4c7e1123b49e09c9025
abigail <Aoboafo@wpi.edu>
2022-04-14 00:35:33 -0400

    laundry service request

---
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/requests/LaundryRequestController.java
@@ -0,0 +1,29 @@
+package edu.wpi.cs3733.D22.teamB.controllers.requests;
+
+import edu.wpi.cs3733.D22.teamB.databases.ServiceRequestsDB;
+import edu.wpi.cs3733.D22.teamB.requests.MealRequest;
+import edu.wpi.cs3733.D22.teamB.requests.MedicineRequest;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LaundryRequestController extends RequestController {
+    public void initialize() {
+        super.initialize();
+    }
+
+    @FXML
+    public void enableSubmission() {
+    }
+
+    @FXML
+    public void sendRequest(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void reset(ActionEvent actionEvent) {
+    }
+
+}
---
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/tables/LaundryController.java
@@ -0,0 +1,4 @@
+package edu.wpi.cs3733.D22.teamB.controllers.tables;
+
+public class LaundryController {
+}
