e1f47deb7a415697a7957c89be38591fc8aaf422
Chase Miller <cpmills26@gmail.com>
2022-04-15 17:38:35 -0400

    Added EquipmentBasedRequestController

--- src/main/java/edu/wpi/cs3733/D22/teamB/requests/LaundryRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/requests/LaundryRequest.java
@@ -1,40 +1,39 @@
 package edu.wpi.cs3733.D22.teamB.requests;
 
-
 import java.util.Date;
 
 public class LaundryRequest extends Request {
 
-    public LaundryRequest(String patientID, String information, int priority) {
-        super(null, patientID, information, priority, "Laundry");
-        this.information = information;
-    }
+  public LaundryRequest(String patientID, String information, int priority) {
+    super(null, patientID, information, priority, "Laundry");
+    this.information = information;
+  }
 
-    public LaundryRequest(
-            String requestID,
-            String employeeID,
-            String locationID,
-            String patientID,
-            String type,
-            String status,
-            int priority,
-            String information,
-            Date timeCreated,
-            Date lastEdited) {
-        super(
-                requestID,
-                employeeID,
-                null,
-                patientID,
-                type,
-                status,
-                priority,
-                information,
-                timeCreated,
-                lastEdited);
-    }
+  public LaundryRequest(
+      String requestID,
+      String employeeID,
+      String locationID,
+      String patientID,
+      String type,
+      String status,
+      int priority,
+      String information,
+      Date timeCreated,
+      Date lastEdited) {
+    super(
+        requestID,
+        employeeID,
+        null,
+        patientID,
+        type,
+        status,
+        priority,
+        information,
+        timeCreated,
+        lastEdited);
+  }
 
-    public final String createRequestID() {
-        return "LAU" + getHashCode();
-    }
+  public final String createRequestID() {
+    return "LAU" + getHashCode();
+  }
 }
