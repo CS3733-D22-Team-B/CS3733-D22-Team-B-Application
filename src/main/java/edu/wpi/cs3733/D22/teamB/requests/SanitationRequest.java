0eb9be8282670a2ba5c5a189177bb38be32a6ba8
Kalina Bonofiglio <kbonofiglio@wpi.edu>
2022-04-15 18:06:23 -0400

    Added send request

--- src/main/java/edu/wpi/cs3733/D22/teamB/requests/SanitationRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/requests/SanitationRequest.java
@@ -1,73 +1,14 @@
 package edu.wpi.cs3733.D22.teamB.requests;
 
 import edu.wpi.cs3733.D22.teamB.databases.DatabaseController;
+import edu.wpi.cs3733.D22.teamB.databases.EquipmentRequest;
 import edu.wpi.cs3733.D22.teamB.databases.MedicalEquipment;
 
-public class SanitationRequest {
+public class SanitationRequest extends EquipmentRequest {
 
     private DatabaseController DC = DatabaseController.getInstance();
-    private MedicalEquipment equipment;
-    private String equipmentID;
-    private String information;
-    private int priority;
 
-    SanitationRequest(String equipmentID, String information, int priority){
-        this.equipmentID = equipmentID;
-        this.information = information;
-        this.priority = priority;
-    }
-
-    public MedicalEquipment getEquipment() {
-        equipment = DC.getMedicalEquipmentByID(equipmentID);
-        return equipment;
-    }
-
-    public void setMedicalEquipment(MedicalEquipment medEq){
-        setEquipmentID(medEq.getEquipmentID());
-    }
-
-    public String getEquipmentID(){
-        return this.equipmentID;
-    }
-
-    private void setEquipmentID(String equipmentID) {
-        this.equipmentID = equipmentID;
-    }
-
-    public String getInformation() {
-        return information;
-    }
-
-    public void setInformation(String information) {
-        this.information = information;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    ////////////// Equipmment //////////////////////////
-    public String getType() {
-        return equipment.getType();
-    }
-
-    public boolean getIsClean() {
-        return equipment.getIsClean();
-    }
-
-    public String getIsSterilized() {
-        return equipment.getIsSterilized();
-    }
-
-    public String getAvailability() {
-        return equipment.getAvailability();
-    }
-
-    public String getName() {
-        return equipment.getName();
+    public SanitationRequest(String equipmentID, String information, int priority) {
+        super(null, equipmentID, information, priority);
     }
 }
