57d83eb647ed4c0d4d9e3c6f161fe3cbb64084c1
Nathan Wong <93850704+nk-wong@users.noreply.github.com>
2022-04-25 23:48:32 -0400

    Initial analytics page setup

--- src/main/java/edu/wpi/cs3733/D22/teamB/controllers/tables/AnalyticsController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/tables/AnalyticsController.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.D22.teamB.controllers.tables;
 
+import edu.wpi.cs3733.D22.teamB.controllers.MenuBarController;
 import edu.wpi.cs3733.D22.teamB.databases.DatabaseController;
 import edu.wpi.cs3733.D22.teamB.databases.MedicalEquipment;
 import java.net.URL;
@@ -7,15 +8,27 @@ import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.PieChart;
+import javafx.scene.chart.*;
 
-public class AnalyticsController implements Initializable {
+public class AnalyticsController extends MenuBarController implements Initializable {
 
-  private ObservableList<PieChart.Data> data;
+  @FXML private StackedBarChart stackedBarChart;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+
+    CategoryAxis xAxis = new CategoryAxis();
+    xAxis.setLabel("Types");
+    NumberAxis yAxis = new NumberAxis();
+    yAxis.setLabel("Units");
+
+    stackedBarChart = new StackedBarChart(xAxis, yAxis);
+
+    XYChart.Series<String, Integer> series1 = new XYChart.Series<String, Integer>();
+    XYChart.Series<String, Integer> series2 = new XYChart.Series<String, Integer>();
+
     LinkedList<MedicalEquipment> medEq = DatabaseController.getInstance().listMedicalEquipment();
 
     int numBedsAvailable = 0;
@@ -61,17 +74,20 @@ public class AnalyticsController implements Initializable {
       }
     }
 
-    data =
-        FXCollections.observableArrayList(
-            new PieChart.Data("Available Beds", numBedsAvailable),
-            new PieChart.Data("Unavailable Beds", numBedsUnavailable),
-            new PieChart.Data("Available Recliners", numReclinersAvailable),
-            new PieChart.Data("Recliners", numReclinersUnavailable),
-            new PieChart.Data("PUMPS", numPumpsAvailable),
-            new PieChart.Data("Unavailable Pumps", numPumpsUnavailable),
-            new PieChart.Data("Available X-Rays", numXraysAvailable));
-    new PieChart.Data("Unavailable X-Rays", numXraysUnavailable);
-    PieChart chart = new PieChart(data);
-    chart.setTitle("Equipment Status");
+    series1.setName("Available");
+    series1.getData().add(new XYChart.Data<>("Beds", numBedsAvailable));
+    series1.getData().add(new XYChart.Data<>("Recliners", numReclinersAvailable));
+    series1.getData().add(new XYChart.Data<>("Pumps", numPumpsAvailable));
+    series1.getData().add(new XYChart.Data<>("X-Rays", numXraysAvailable));
+
+    series2.setName("Unavailable");
+    series2.getData().add(new XYChart.Data<>("Beds", numBedsUnavailable));
+    series2.getData().add(new XYChart.Data<>("Recliners", numReclinersUnavailable));
+    series2.getData().add(new XYChart.Data<>("Pumps", numPumpsUnavailable));
+    series2.getData().add(new XYChart.Data<>("X-Rays", numXraysUnavailable));
+
+    stackedBarChart.setTitle("Medical Equipment");
+    stackedBarChart.setCategoryGap(50);
+    stackedBarChart.getData().addAll(series1, series2);
   }
 }
