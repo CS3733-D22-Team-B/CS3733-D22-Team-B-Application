bc92dd555dc494bb4b4126bcfc1bb07bce34108f
Chase Miller <cpmills26@gmail.com>
2022-04-26 20:51:20 -0400

    Something works

--- src/main/java/edu/wpi/cs3733/D22/teamB/controllers/HeatMapController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamB/controllers/HeatMapController.java
@@ -1,49 +1,139 @@
 package edu.wpi.cs3733.D22.teamB.controllers;
 
+import edu.wpi.cs3733.D22.teamB.databases.*;
+import edu.wpi.cs3733.D22.teamB.requests.Request;
 import eu.hansolo.fx.charts.heatmap.*;
 import eu.hansolo.fx.charts.tools.ColorMapping;
+import eu.hansolo.fx.charts.tools.Point;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 
 public class HeatMapController {
   @FXML private StackPane pane;
   private HeatMap heatMap;
 
+  private int floorNumber = 2;
+  private String floor = "1";
+
+  private List<Point> locationPoints;
+
   public void initialize() {
     heatMap =
         HeatMapBuilder.create()
-            .prefSize(400, 400)
-            .colorMapping(ColorMapping.LIME_YELLOW_RED)
-            .spotRadius(20)
+            .prefSize(540, 960)
+            .colorMapping(ColorMapping.BLUE_CYAN_GREEN_YELLOW_RED)
+            .spotRadius(25)
             .opacityDistribution(OpacityDistribution.CUSTOM)
             .fadeColors(true)
             .build();
 
-    heatMap.setOnMouseMoved(e -> heatMap.addSpot(e.getX(), e.getY()));
-
     pane.getChildren().add(heatMap);
+  }
 
-    pane.addEventFilter(
-        MouseEvent.MOUSE_MOVED,
-        event -> {
-          double x = event.getX();
-          double y = event.getY();
-          if (x < heatMap.getSpotRadius()) x = heatMap.getSpotRadius();
-          if (x > pane.getWidth() - heatMap.getSpotRadius())
-            x = pane.getWidth() - heatMap.getSpotRadius();
-          if (y < heatMap.getSpotRadius()) y = heatMap.getSpotRadius();
-          if (y > pane.getHeight() - heatMap.getSpotRadius())
-            y = pane.getHeight() - heatMap.getSpotRadius();
+  @FXML
+  public void increaseFloor(ActionEvent event) {
+    if (floorNumber < 6) {
+      floorNumber++;
+    }
 
-          heatMap.addSpot(x, y);
-        });
-    pane.widthProperty()
-        .addListener(
-            (ov, oldWidth, newWidth) -> heatMap.setSize(newWidth.doubleValue(), pane.getHeight()));
-    pane.heightProperty()
-        .addListener(
-            (ov, oldHeight, newHeight) ->
-                heatMap.setSize(pane.getWidth(), newHeight.doubleValue()));
+    switch (floorNumber) {
+      case 1:
+        floor = "L1";
+        break;
+      case 2:
+        floor = "1";
+        break;
+      case 3:
+        floor = "2";
+        break;
+      case 4:
+        floor = "3";
+        break;
+      case 5:
+        floor = "4";
+        break;
+      case 6:
+        floor = "5";
+        break;
+    }
+
+    drawHeatCircle();
+  }
+
+  @FXML
+  public void decreaseFloor(ActionEvent event) {
+    if (floorNumber > 0) {
+      floorNumber--;
+    }
+
+    switch (floorNumber) {
+      case 0:
+        floor = "L2";
+        break;
+      case 1:
+        floor = "L1";
+        break;
+      case 2:
+        floor = "1";
+        break;
+      case 3:
+        floor = "2";
+        break;
+      case 4:
+        floor = "3";
+        break;
+      case 5:
+        floor = "4";
+        break;
+    }
+
+    drawHeatCircle();
+  }
+
+  private void drawHeatCircle() {
+    //    ArrayList<Point> points = new ArrayList<Point>();
+    //
+    //    for (int i = 0; i < 1000; i++) {
+    //      double rand = Math.random() * 2 * Math.PI;
+    //
+    //      double x1 = Math.cos(rand) * 100 + 300;
+    //      double y1 = Math.sin(rand) * 100 + 150;
+    //
+    //      points.add(new Point(x1, y1));
+    //    }
+
+    locationPoints = new ArrayList<Point>();
+    clear();
+
+    ActivityDB db = ActivityDB.getInstance();
+
+    for (Activity activity : db.list()) {
+      if (activity.getType().equals("Location")) {
+        Location location = LocationsDB.getInstance().getLocation(activity.getTypeID());
+
+        if (location != null && location.getFloor().equals(floor)) {
+          locationPoints.add(new Point(location.getXCoord() * 0.5, location.getYCoord() * 0.5));
+        }
+      } else if (activity.getType().equals("Request")) {
+        Request request = ServiceRequestsDB.getInstance().getByID(activity.getTypeID());
+
+        if (request != null) {
+          Location location = LocationsDB.getInstance().getLocation(request.getLocationID());
+
+          if (location != null && location.getFloor().equals(floor)) {
+            locationPoints.add(new Point(location.getXCoord() * 0.5, location.getYCoord() * 0.5));
+          }
+        }
+      }
+    }
+
+    heatMap.addSpots(locationPoints);
+  }
+
+  private void clear() {
+    heatMap.clearHeatMap();
   }
 }
